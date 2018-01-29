f5d39bd7b04087cfc33d5637831d2029bf742fb4
AlexanderLuo <496952252@qq.com>
2018-01-29 14:42:48 +0800

    23

---
+++ src/main/java/com/alex/App.java
@@ -0,0 +1,20 @@
+package com.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(){
+        return "HELLO BOY";
+    }
+}
