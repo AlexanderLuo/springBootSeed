80e3e62e881a84684d2e4806bb7e941aa64cc0c4
AlexanderLuo <496952252@qq.com>
2018-02-03 17:52:35 +0800

    678976976qwdwqd

--- web/src/main/java/com/alex/controller/Test.java
+++ web/src/main/java/com/alex/controller/Test.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
     @Autowired
+
     IOriSqlService iUserService;
 
     @GetMapping(value = "/hi")
