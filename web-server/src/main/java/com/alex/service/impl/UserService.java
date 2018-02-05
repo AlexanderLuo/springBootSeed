035901ba074d2c6ad434988bbe11fe62ba4d1743
AlexanderLuo <tianchuiqin@gmail.com>
2018-02-05 18:12:48 +0800

    346236

--- web-server/src/main/java/com/alex/service/impl/UserService.java
+++ web-server/src/main/java/com/alex/service/impl/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends ServiceImpl<UserMapper, User> implements IUserService{
+    @Autowired
+    UserMapper userMapper;
 
 
     @Override
